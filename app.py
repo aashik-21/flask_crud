e4def3bb308fd72215d09ec66439c949705b2b22
aashik-21 <aashikm460@gmail.com>
2025-04-14 12:27:25 +0530

    35:4

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ db.init_app(app)
 # @app.before_first_request
 def create_table():
     db.create_all()
-
 with app.app_context():
     create_table()
 
@@ -19,6 +18,35 @@ with app.app_context():
 def create():
     if request.method == "GET":
         return render_template("create.html")
-   
 
+
+    if request.method == "POST":
+        hobby = request.form.getlist("hobbies")
+        hobbies = ",".join(map(str, hobby))
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        gender = request.form["gender"]
+        hobbies = hobbies
+        country = request.form["country"]
+
+        students = StudentModel(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+            gender=gender,
+            hobbies=hobbies,
+            country=country
+        )
+        db.session.add(student)
+        db.session.commit()
+        return redirect("/")
+
+@app.route('/', methods =['GET','POST'])
+def RetrieveList():
+    students = StudentModel.query.all()
+    return render_template("index.html",students=students)
+    
 app.run(host='localhost', port=5000 )
